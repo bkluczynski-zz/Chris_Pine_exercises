fb394a7da09f989a2a7fee6464884035e79153bd
Bart <kluczynskib@gmail.com>
2016-06-13 19:17:11 +0100

    Adding first exercise

---
+++ seconds_to_year.rb
@@ -0,0 +1,9 @@
+def seconds_to_years_converter(seconds)
+  
+ converter = seconds/60/60/24/365
+ 
+  puts "Your current age is #{converter} years"
+   
+  end
+   
+seconds_to_years_converter(1100000000)
